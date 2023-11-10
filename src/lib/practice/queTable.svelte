17f53f197d2ada701ce5cd9822e20e15fedfd6e9
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-11-11 01:10:31 +0530

    Added Question Tags

---
+++ src/lib/practice/queTable.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Bell, EyeNone, Person } from 'radix-icons-svelte';
+	import * as Card from '$ui/card';
+</script>
+
+<Card.Root>
+	<Card.Content class="py-2 ">
+        <h1>Code</h1>
+    </Card.Content>
+</Card.Root>
---
+++ src/routes/practice/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import QueTable from '$lib/practice/queTable.svelte';
+import TagsSidebar from '$lib/practice/tagsSidebar.svelte';
+	import * as Card from '$ui/card';
+	import { Bell, EyeNone, Person } from 'radix-icons-svelte';
+</script>
+
+<main class="flex flex-col md:flex-row gap-6 m-3 my-6 md:m-8">
+	<div class="w-full md:w-96">
+		<TagsSidebar />
+	</div>
+	<div class="w-full">
+		<QueTable/>
+	</div>
+</main>
